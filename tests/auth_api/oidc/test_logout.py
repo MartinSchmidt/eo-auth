5a8ba9dea64b37e7d36497dc00570dda6cc270c0
Claus <claus.christiansen1987@gmail.com>
2022-01-30 15:55:58 +0100

    Started integration test impl.

--- tests/auth_api/oidc/test_logout.py
+++ tests/auth_api/oidc/test_logout.py
@@ -5,6 +5,7 @@ import pytest
 from typing import Any, Dict
 from datetime import datetime, timezone
 
+from unittest.mock import MagicMock
 from flask.testing import FlaskClient
 from urllib.parse import parse_qs, urlsplit
 
@@ -20,6 +21,8 @@ from origin.api.testing import (
     assert_query_parameter,
 )
 
+from auth_api.config import OIDC_API_LOGOUT_URL
+
 from auth_api.config import (
     TOKEN_COOKIE_DOMAIN,
     TOKEN_COOKIE_HTTP_ONLY,
@@ -32,109 +35,51 @@ from .bases import OidcCallbackEndpointsSubjectKnownBase
 
 
 # -- Helpers -----------------------------------------------------------------
+from ..conftest import mock_get_jwk, mock_fetch_token, jwk_public, ip_token
 
 
-def assert_token(
-        client: FlaskClient,
-        opaque_token: str,
-        expected_token: Dict[str, Any],
-):
+class Testlogout:
     """
-    Provided an opaque token, this function translates it to an
-    internal token and asserts on it's content.
-
-    :param client:
-    :param opaque_token:
-    :param expected_token:
-    :return:
-    """
-    client.set_cookie(
-        server_name=TOKEN_COOKIE_DOMAIN,
-        key=TOKEN_COOKIE_NAME,
-        value=opaque_token,
-        secure=True,
-        httponly=TOKEN_COOKIE_HTTP_ONLY,
-        samesite='Strict',
-    )
-
-    r_forwardauth = client.get('/token/forward-auth')
-
-    r_inspect = client.get(
-        path='/token/inspect',
-        headers={TOKEN_HEADER_NAME: r_forwardauth.headers[TOKEN_HEADER_NAME]}
-    )
-
-    assert r_inspect.status_code == 200
-    assert r_inspect.json == {'token': expected_token}
-
-
-# -- Fixtures ----------------------------------------------------------------
-
-
-@pytest.fixture(params=[
-    OIDC_LOGIN_CALLBACK_PATH,
-    OIDC_SSN_VALIDATE_CALLBACK_PATH,
-])
-def callback_endpoint_path(request) -> str:
-    """
-    Returns path to callback endpoint.
-    """
-    return request.param
-
-
-# -- Tests -------------------------------------------------------------------
-
-class TestOidcLogout(OidcCallbackEndpointsSubjectKnownBase):
-    """
-    todo
+    Tests cases where the user accepts the terms and conditions
     """
 
     @pytest.mark.integrationtest
-    def test__should_register_user_login(
-            self,
-            client: FlaskClient,
-            mock_session: db.Session,
-            callback_endpoint_path: str,
-            internal_subject: str,
-            return_url: str,
-            state_encoded: str,
+    def test__user_logout(
+        self,
+        client: FlaskClient,
+        mock_session: db.Session,
+        mock_get_jwk: MagicMock,
+        mock_fetch_token: MagicMock,
+        state_encoder: TokenEncoder[AuthState],
+        jwk_public: str,
+        ip_token: Dict[str, Any],
+        token_issued: datetime,
+        token_expires: datetime,
+        userinfo_token: str,
     ):
+        # -- Arrange ----------------------------------------------------------
 
-        # -- Act -------------------------------------------------------------
 
-        # Login
-        r1 = client.get(
-            path=callback_endpoint_path,
-            query_string={'state': state_encoded},
+        state = AuthState(
+            fe_url='http://foobar.com',
+            return_url='http://redirect-here.com/foobar',
         )
 
-        # 
-        cookie_name = 'Authorization'
+        state_encoded = state_encoder.encode(state)
+        mock_get_jwk.return_value = jwk_public
+        mock_fetch_token.return_value = ip_token
 
-        cookies = CookieTester(r1.headers)
+        print(userinfo_token)
+        # -- Act --------------------------------------------------------------
 
-        cookie_value = cookies.get_value(TOKEN_COOKIE_NAME)
-
-        client.set_cookie(
-            server_name='domain.com',
-            key=cookie_name,
-            value=cookie_value
-        )
-
-        r2 = client.get(
-            path='/token/forward-auth',
-        )
-
-        r3 = client.get(
-            path='/logout',
-            headers={
-                'Authorization': r2.headers['Authorization']
+        r = client.post(
+            path=OIDC_API_LOGOUT_URL,
+            json={
+                'id_token': ip_token['id_token']
             }
         )
 
-        # -- Assert ----------------------------------------------------------
 
+        # -- Assert -----------------------------------------------------------
 
-        # Check that cookie has been deleted
-
-        # Check that oidc logout endpoint has been touched
+        assert r.status_code == 400
