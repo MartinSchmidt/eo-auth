bd5c9f83204325ffee6b47adb4e7f45831b81a3b
Claus <45032018+C-Christiansen@users.noreply.github.com>
2022-02-01 14:59:00 +0100

    updated test file logout

--- tests/auth_api/oidc/test_logout.py
+++ tests/auth_api/oidc/test_logout.py
@@ -20,6 +20,7 @@ from origin.api.testing import (
     assert_base_url,
     assert_query_parameter,
 )
+from auth_api.models import DbUser, DbExternalUser
 
 from auth_api.config import OIDC_API_LOGOUT_URL
 
@@ -37,12 +38,35 @@ from .bases import OidcCallbackEndpointsSubjectKnownBase
 # -- Helpers -----------------------------------------------------------------
 from ..conftest import mock_get_jwk, mock_fetch_token, jwk_public, ip_token
 
+"""
+Tests to be done:
+1. Test db_controller.get_token()
 
-class Testlogout:
-    """
-    Tests cases where the user accepts the terms and conditions
-    """
+2. Test at token_record gets deleted
 
+3. Test that only the correct token_record gets deleted
+
+4. Test that signaturgruppen's api gets called with correct token_record
+4.1 Test that signaturgruppen does not get called if token_record does not exists
+
+5. Test that a new cookie is returned with correct params
+- Is expired
+- value is empty
+- token name is correct
+- path is correct
+
+6. Test Http response body
+6.1 Success=true when success
+- status code is correct
+6.2 Success=false when failed
+- status code is correct
+
+"""
+
+class Testlogout(OidcCallbackEndpointsSubjectKnownBase):
+    """
+    Tests cases where the user is logged in and should log out
+    """
     @pytest.mark.integrationtest
     def test__user_logout(
         self,
@@ -50,28 +74,35 @@ class Testlogout:
         mock_session: db.Session,
         mock_get_jwk: MagicMock,
         mock_fetch_token: MagicMock,
-        state_encoder: TokenEncoder[AuthState],
         jwk_public: str,
         ip_token: Dict[str, Any],
-        token_issued: datetime,
-        token_expires: datetime,
-        userinfo_token: str,
-    ):
-        # -- Arrange ----------------------------------------------------------
+        token_subject: str,
+        token_idp: str,
+        token_ssn: str,
+        #id_token_encoded: str,
+        #internal_subject: str,
+    ) -> db.Session:
 
+        # -- OAuth2Session object methods ------------------------------------
 
-        state = AuthState(
-            fe_url='http://foobar.com',
-            return_url='http://redirect-here.com/foobar',
-        )
-
-        state_encoded = state_encoder.encode(state)
         mock_get_jwk.return_value = jwk_public
         mock_fetch_token.return_value = ip_token
 
-        print(userinfo_token)
-        # -- Act --------------------------------------------------------------
+        # -- Insert user into database ---------------------------------------
 
+        """
+        print(id_token_encoded)
+        mock_session.add(DbUser(
+            subject=internal_subject,
+            ssn=token_ssn,
+        ))
+
+        mock_session.add(DbExternalUser(
+            subject=internal_subject,
+            identity_provider=token_idp,
+            external_subject=token_subject,
+        ))
+        """
         r = client.post(
             path=OIDC_API_LOGOUT_URL,
             json={
