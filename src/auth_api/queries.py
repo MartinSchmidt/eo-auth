14ad43d9b1fd0733a0bcf94bd047f2610d9d4614
rasmus godske <rasmusgodske@gmail.com>
2022-02-16 12:38:47 +0000

    fixed queries.py linting issues

--- src/auth_api/queries.py
+++ src/auth_api/queries.py
@@ -6,26 +6,25 @@ from .models import DbUser, DbExternalUser, DbToken, DbLoginRecord
 
 
 class UserQuery(SqlQuery):
-    """
-    Query DbUser.
-    """
+    """Query DbUser."""
+
     def _get_base_query(self) -> orm.Query:
-        """
-        TODO
-        """
+        """Override function used in base class."""
+
         return self.session.query(DbUser)
 
     def has_ssn(self, ssn: str) -> 'UserQuery':
         """
-        Check if the actor's ssn matches a ssn in the database
+        Check if the actor's ssn matches a ssn in the database.
 
         :param ssn: Social security number, encrypted
         """
+
         return self.filter(DbUser.ssn == ssn)
 
     def has_tin(self, tin: str) -> 'UserQuery':
         """
-        Check if the subject's tin matches a tin in the database
+        Check if the subject's tin matches a tin in the database.
 
         :param tin: Tax Identification Number
         """
@@ -33,73 +32,70 @@ class UserQuery(SqlQuery):
 
 
 class ExternalUserQuery(SqlQuery):
-    """
-    Query DbExternalUser.
-    """
+    """Query DbExternalUser."""
+
     def _get_base_query(self) -> orm.Query:
-        """
-        TODO
-        """
+        """Override function used in base class."""
+
         return self.session.query(DbExternalUser)
 
     def has_external_subject(self, subject: str) -> 'ExternalUserQuery':
         """
-        Check if the external subject exists in the database
+        Check if the external subject exists in the database.
 
         :param subject: ID/Name of the external subject
         """
+
         return self.filter(DbExternalUser.external_subject == subject)
 
-    def has_identity_provider(self, ip: str) -> 'ExternalUserQuery':
+    def has_identity_provider(self, idp: str) -> 'ExternalUserQuery':
         """
-        Checks if the subject has the given identity provider in the database.
+        Check if the subject has the given identity provider in the database.
 
-        :param ip: ID/name of Identity Provider
+        :param idp: ID/name of Identity Provider
         """
-        return self.filter(DbExternalUser.identity_provider == ip)
+
+        return self.filter(DbExternalUser.identity_provider == idp)
 
 
 class LoginRecordQuery(SqlQuery):
-    """
-    Query DbLoginRecord.
-    """
+    """Query DbLoginRecord."""
+
     def _get_base_query(self) -> orm.Query:
-        """
-        TODO
-        """
+        """Override function used in base class."""
+
         return self.session.query(DbLoginRecord)
 
     def has_subject(self, subject: str) -> 'LoginRecordQuery':
         """
-        Check if the subject exists in the database
+        Check if the subject exists in the database.
 
         param subject: ID/Name of the subject
         """
+
         return self.filter(DbLoginRecord.subject == subject)
 
 
 class TokenQuery(SqlQuery):
-    """
-    Query DbToken.
-    """
+    """Query DbToken."""
+
     def _get_base_query(self) -> orm.Query:
-        """
-        TODO
-        """
+        """Override function used in base class."""
+
         return self.session.query(DbToken)
 
     def has_opaque_token(self, opaque_token: str) -> 'TokenQuery':
         """
-        Check if the opaque token exists in the database
+        Check if the opaque token exists in the database.
 
         param opaque_token: Primary Key Constraint
         """
+
         return self.filter(DbToken.opaque_token == opaque_token)
 
     def is_valid(self) -> 'TokenQuery':
-        """
-        Check if the token has a correct issued and expires datetime
-        """
+        """Check if the token has a correct issued and expires datetime."""
+
         return self.filter(and_(
             DbToken.issued <= func.now(),
             DbToken.expires > func.now(),
