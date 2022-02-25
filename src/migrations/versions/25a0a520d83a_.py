c80a93ea1882ef5e155367ca4d91a874b4c140bd
Claus <45032018+C-Christiansen@users.noreply.github.com>
2022-02-25 11:33:01 +0100

    Changed migration back

--- src/migrations/versions/25a0a520d83a_.py
+++ src/migrations/versions/25a0a520d83a_.py
@@ -1,9 +1,7 @@
 """empty message
-
 Revision ID: 25a0a520d83a
-Revises: 
+Revises:
 Create Date: 2021-11-12 11:45:29.153816
-
 """
 from alembic import op
 import sqlalchemy as sa
@@ -43,13 +41,13 @@ def upgrade():
     sa.Column('subject', sa.String(), nullable=False),
     sa.Column('created', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=False),
     sa.Column('ssn', sa.String(), nullable=True),
-    sa.Column('tin', sa.String(), nullable=True),
-    sa.CheckConstraint('ssn != NULL OR tin != null'),
+    sa.Column('cvr', sa.String(), nullable=True),
+    sa.CheckConstraint('ssn != NULL OR cvr != null'),
     sa.PrimaryKeyConstraint('subject'),
     sa.UniqueConstraint('ssn'),
     sa.UniqueConstraint('subject')
     )
-    op.create_index(op.f('ix_user_tin'), 'user', ['tin'], unique=False)
+    op.create_index(op.f('ix_user_cvr'), 'user', ['cvr'], unique=False)
     op.create_index(op.f('ix_user_ssn'), 'user', ['ssn'], unique=False)
     op.create_index(op.f('ix_user_subject'), 'user', ['subject'], unique=False)
     op.create_table('user_external',
@@ -78,7 +76,7 @@ def downgrade():
     op.drop_table('user_external')
     op.drop_index(op.f('ix_user_subject'), table_name='user')
     op.drop_index(op.f('ix_user_ssn'), table_name='user')
-    op.drop_index(op.f('ix_user_tin'), table_name='user')
+    op.drop_index(op.f('ix_user_cvr'), table_name='user')
     op.drop_table('user')
     op.drop_index(op.f('ix_token_subject'), table_name='token')
     op.drop_index(op.f('ix_token_opaque_token'), table_name='token')
